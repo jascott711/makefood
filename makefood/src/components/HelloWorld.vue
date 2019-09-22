b121734f4ea7cb799971f1b3196b1c7ee35950ff
Jesse <ja.scott711@gmail.com>
2019-09-22 12:09:25 -0400

    initial commit

---
+++ makefood/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize your kitchen skills!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'makefood',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #000;
+  font-size: 42px;
+  font-weight: 700;
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+p {
+  font-size: 18px;
+  color: #333;
+}
+</style>
